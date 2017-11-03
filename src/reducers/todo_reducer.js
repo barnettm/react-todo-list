e5eff0de3c5bdc88c0715447bc453aacf82c0e54
barnettm <barnett717@gmail.com>
2017-11-02 17:51:31 -0700

    merge conflicts

--- src/reducers/todo_reducer.js
+++ src/reducers/todo_reducer.js
@@ -3,14 +3,10 @@ import types from '../actions/types';
 const DEFAULT_STATE = { all: [] }
 
 export default(state = DEFAULT_STATE, action) => {
-    switch(action.type){
+    switch (action.type) {
         case types.GET_ALL:
             console.log("GET ALL: ", action.payload)
-            return {all: action.payload.data.todos}
-        // case types.ADD_TODO:
-        //     // return {all: [action.payload, ...state.all]};
-        //     console.log("ADD_TODO: ", action.payload)
-        //     return state;       
+            return {all: action.payload.data.todos}       
         default: 
             return state;
     }
